dfdbd7be4af8360c08ade12ef07166f03f5efa4e
zhangquan <zhangquan1015@gmail.com>
2022-02-25 09:36:05 +0800

    proj0

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,4 +1,4 @@
-public  class Body{
+public  class Planet{
     public double xxPos;//Its current x position
     public double yyPos;//Its current y position
     public double xxVel;//Its current velocity in the x direction
@@ -6,7 +6,7 @@ public  class Body{
     public double mass;//Its mass
     public String imgFileName;//The name of the file that corresponds to the image that depicts the body
 
-    public Body(double xP,double yP,double xV,double yV,double m,String img){
+    public Planet(double xP,double yP,double xV,double yV,double m,String img){
         this.xxPos=xP;
         this.yyPos=yP;
         this.xxVel=xV;
@@ -14,7 +14,7 @@ public  class Body{
         this.mass=m;
         this.imgFileName=img;
     }   
-    public Body(Body b){
+    public Planet(Planet b){
         this.xxPos=b.xxPos;
         this.yyPos=b.yyPos;
         this.xxVel=b.xxVel;
@@ -23,48 +23,48 @@ public  class Body{
         this.imgFileName=b.imgFileName;
     }
     
-    public double xxDis;//The distance between Bodya and Bodyb in x
-    public double yyDis;//The distance between Bodya and Bodyb in y
+    public double xxDis;//The distance between Planeta and Planetb in x
+    public double yyDis;//The distance between Planeta and Planetb in y
     
-    public double calcDistance(Body b){
+    public double calcDistance(Planet b){
         xxDis=b.xxPos-this.xxPos;
         yyDis=b.yyPos-this.yyPos;
         return Math.sqrt(Math.pow(xxDis,2)+Math.pow(yyDis,2));
     }
     
     public static double G=6.67e-11;
-    public double calcForceExertedBy(Body b){
+    public double calcForceExertedBy(Planet b){
         return G*this.mass*b.mass/Math.pow(this.calcDistance(b),2);
     }
 
-    public double calcForceExertedByX(Body b){
+    public double calcForceExertedByX(Planet b){
         xxDis=b.xxPos-this.xxPos;
         return this.calcForceExertedBy(b)*xxDis/this.calcDistance(b);
     }
 
-    public double calcForceExertedByY(Body b){
+    public double calcForceExertedByY(Planet b){
         yyDis=b.yyPos-this.yyPos;
         return this.calcForceExertedBy(b)*yyDis/this.calcDistance(b);
     }
 
-    public double calcNetForceExertedByX(Body[] allBodys){
-        int len=allBodys.length;
+    public double calcNetForceExertedByX(Planet[] allPlanets){
+        int len=allPlanets.length;
         double NetForceX=0;
         for(int i=0;i<len;i++){
-            if(this.equals(allBodys[i]))
+            if(this.equals(allPlanets[i]))
                 continue;
-            NetForceX+=this.calcForceExertedByX(allBodys[i]);
+            NetForceX+=this.calcForceExertedByX(allPlanets[i]);
         }
         return NetForceX;
     }
 
-    public double calcNetForceExertedByY(Body[] allBodys){
-        int len=allBodys.length;
+    public double calcNetForceExertedByY(Planet[] allPlanets){
+        int len=allPlanets.length;
         double NetForceY=0;
         for(int i=0;i<len;i++){
-            if(this.equals(allBodys[i]))
+            if(this.equals(allPlanets[i]))
                 continue;
-            NetForceY+=this.calcForceExertedByY(allBodys[i]);
+            NetForceY+=this.calcForceExertedByY(allPlanets[i]);
         }
         return NetForceY;
     }
@@ -77,7 +77,7 @@ public  class Body{
         this.xxPos+=this.xxVel*dt;
         this.yyPos+=this.yyVel*dt;
     }
-    //Drawing One Body
+    //Drawing One Planet
     public void draw(){
         String path="images/";//the Relative Path
         path+=this.imgFileName;
