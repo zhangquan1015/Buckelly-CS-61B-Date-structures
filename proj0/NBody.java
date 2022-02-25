553600b7c3067ae35883e3898baba1bad32891c0
zhangquan <zhangquan1015@gmail.com>
2022-02-25 09:18:21 +0800

    be ready for Submission

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -29,23 +29,49 @@ public class NBody{
         return b1;
     }
 
-    //Drawing the Initial Universe State
     public static void main(String[] args){
         double T=Double.parseDouble(args[0]);
         double dt=Double.parseDouble(args[1]);
+
+        String path="images/starfield.jpg";
         String filename=args[2];
         R=NBody.readRadius(filename);
         Body[] b=NBody.readBodies(filename);
-        //Drawing the Background
-        StdDraw.enableDoubleBuffering();
-        StdDraw.setScale(-R,R);
-        String path="images/starfield.jpg";
-        StdDraw.picture(0,0,path);
-        //Drawing More than One Body
-        for(int i=0;i<N;i++){
-            b[i].draw();
+        
+        //Creating an Animation
+        for(double t=0;t<T;t++){
+            double[] xForces=new double[N];
+            double[] yForces=new double[N];
+            for(int j=0;j<N;j++){
+                xForces[j]=b[j].calcNetForceExertedByX(b);
+                yForces[j]=b[j].calcNetForceExertedByY(b);
+            }
+            for(int k=0;k<N;k++){
+                b[k].update(dt,xForces[k],yForces[k]);
+            }
+            
+            StdDraw.clear();
+            StdDraw.enableDoubleBuffering();
+            StdDraw.setScale(-R,R);
+
+            StdDraw.enableDoubleBuffering();
+            StdDraw.setScale(-R,R);
+            StdDraw.picture(0,0,path);
+            //Drawing More than One Body
+            for(int i=0;i<N;i++){
+                b[i].draw();
+            }
+            StdDraw.show();
+            StdDraw.pause(10);
         }
-        StdDraw.show();
-        StdDraw.pause(2000);
+
+        //Printing the Universe
+        StdOut.printf("%d\n",N);
+        StdOut.printf("%.2e\n",R);
+        for(int i=0;i<N;i++){
+            System.out.printf("%11.4e,%11.4e,%11.4e,%11.4e,%11.4e,%12s\n",
+                  b[i].xxPos, b[i].yyPos, b[i].xxVel,
+                  b[i].yyVel, b[i].mass, b[i].imgFileName);
+        }
     }
 }
